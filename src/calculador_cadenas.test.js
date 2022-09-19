18f732bcea45de6a02152ad78854fb2b62ce0621
Alfred <alfred.garcia@ucb.edu.bo>
2022-09-18 23:24:05 -0400

    Toma en cuenta cadena con varios números

--- src/calculador_cadenas.test.js
+++ src/calculador_cadenas.test.js
@@ -12,4 +12,12 @@ describe("!!! CALCULA CADENAS !!!", () => {
   it("deberia retornar 2 al ingresar la cadena 1,1 ", () => {
     expect(calculador("1,1")).toEqual(2);
   });
+
+  it("deberia retornar 3 al ingresar la cadena 1,1,1 ", () => {
+    expect(calculador("1,1,1")).toEqual(3);
+  });
+  
+  it("deberia retornar 6 al ingresar la cadena 1,2,3 ", () => {
+    expect(calculador("1,2,3")).toEqual(6);
+  });
 });
