8e755e92bd8b1e03b4db1d5470d8742bcaa66146
Alfred <alfred.garcia@ucb.edu.bo>
2022-09-18 23:32:31 -0400

    Calcula suma de la cadena separada con '-'

--- src/calculador_cadenas.js
+++ src/calculador_cadenas.js
@@ -1,16 +1,16 @@
 function calculador(cadena) {
   var res = 0;
-
+  
   if(cadena === ""){
     res = 0;
   }
-  else if(cadena.search(",") != -1) {
-    const cadenaArray = cadena.split(",");
+  else if(cadena.search(",") != -1 || cadena.search("-") != -1) {
+    const cadenaArray = cadena.split(/[,-]+/);
     for(var i = 0; i < cadenaArray.length ; i++){
       res = res + parseInt(cadenaArray[i]);
     }
   } else res = parseInt(cadena);
-
+  
   return res;
 }
 
